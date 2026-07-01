1f1f4c11f90c9a77880f46eb19ed5bf8d851e6cb
QinyRui <eqinivd19@163.com>
2026-07-01 15:54:31 +0800

    Upload index.ts via iOS Scripting

---
+++ ui/index.ts
@@ -0,0 +1,10 @@
+/**
+ * UI 模块入口
+ * 统一导出所有 UI 组件
+ */
+
+export { MainPage } from './main-page'
+export { ResultPopup } from './result-popup'
+export { AboutView } from './about-view'
+export { SettingsPage } from './settings-page'
+export { IconBadge, GlowIcon, Row, CardSection, MIHOYO_ICON_URL } from './components'
